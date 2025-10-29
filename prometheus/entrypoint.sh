443e606a9b811839e68ffbd9d2e46229611ddd1c
oliveres <oldrich.sveda@gmail.com>
2025-10-29 12:08:30 +0100

    Use multi-stage build for Prometheus config substitution

--- prometheus/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Substitute environment variables in prometheus config
-envsubst < /etc/prometheus/prometheus.yml.template > /etc/prometheus/prometheus.yml
-
-# Start Prometheus with all arguments passed to this script
-exec /bin/prometheus "$@"
