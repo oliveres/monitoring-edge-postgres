507755ed250e105809f1a68ecfab4f1759e3e41d
oliveres <oldrich.sveda@gmail.com>
2025-10-29 11:59:51 +0100

    Fix Promtail: use native -config.expand-env flag instead of envsubst

--- promtail/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Substitute environment variables in promtail config
-envsubst < /etc/promtail/promtail-config.yaml.template > /etc/promtail/promtail-config.yaml
-
-# Start Promtail with all arguments passed to this script
-exec /usr/bin/promtail "$@"
